67bd0789c6ca9d34acf6e8c566b41b9be4674b40
Ayushi Shah <as17726@nyu.edu>
2023-10-25 16:38:05 -0400

    Update sketch.js

--- sound/sketch.js
+++ sound/sketch.js
@@ -7,8 +7,31 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   print(song.duration());
+  song.playMode("restart"); 
 }
 
+
 function draw() {
-  background(220, 20, 120);
+  if(song.isPlaying()) {
+    background(20, 220, 120); 
+  } else if (song.isPaused()) {
+    background(20,220,120)
+  } else {
+  background(220, 20, 120); }
+}
+
+function mouseClicked() {
+  song.play(); 
+}
+
+function keyReleased() {
+  if (key == "s") {
+    song.stop();
+  } else if (key == "p") {
+    if(song.isPlaying()) {
+      song.pause();
+    } else {
+      song.play(); 
+    }
+  }
 }
